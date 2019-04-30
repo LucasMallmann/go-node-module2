36db117af213d5a03918f81183f78e88798c16d7
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-04-29 23:01:49 -0300

    Desafion go-node-module-2 finalizado! Provider verificar seus agendamentos

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,7 +1,8 @@
-const { Appointment } = require('../models')
+const moment = require('moment')
+const { Appointment, User } = require('../models')
 
 class ProviderController {
-  async index(req, res) {
+  async index (req, res) {
     const { id } = req.session.user
     const appointments = await Appointment.findAll({
       where: {
@@ -9,7 +10,22 @@ class ProviderController {
       }
     })
 
-    return res.render('provider/index', { appointments })
+    const promises = appointments.map(async appointment => {
+      const appointmentDateUTC = moment(appointment.date).format()
+      const localDate = moment.utc(appointmentDateUTC).local()
+      const user = await User.findByPk(appointment.user_id)
+
+      return {
+        day: localDate.format('DD/MM/YY'),
+        hour: localDate.format('HH:mm'),
+        user,
+        appointment: appointment.id
+      }
+    })
+
+    const providerAppointments = await Promise.all(promises)
+
+    return res.render('provider/index', { providerAppointments })
   }
 }
 
